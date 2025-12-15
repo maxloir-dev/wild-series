fb4cb1e7a28970a95ac4ea96a86128abc60d399b
Maxime Loir <maxloir@hotmail.fr>
2025-12-15 15:55:04 +0100

    router

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,14 +9,6 @@ import "../database/checkConnection";
 // Import the Express application from ./app
 import app from "./app";
 
-import type { RequestHandler } from "express";
-
-const sayWelcome: RequestHandler = (req, res) => {
-  res.send("Welcome to Wild Series !");
-};
-
-app.get("/", sayWelcome);
-
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
 
--- server/src/router.ts
+++ server/src/router.ts
@@ -15,4 +15,12 @@ router.post("/api/items", itemActions.add);
 
 /* ************************************************************************* */
 
+import type { RequestHandler } from "express";
+
+const sayWelcome: RequestHandler = (req, res) => {
+  res.send("Welcome to Wild Series !");
+};
+
+router.get("/", sayWelcome);
+
 export default router;
